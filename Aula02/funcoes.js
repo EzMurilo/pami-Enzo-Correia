468b6caf4ea067c91290fe7004b4b05002b56d42
EzMurilo <enzomurilo321@gmail.com>
2026-09-10 21:56:31 -0300

    Aula sobre funções em Java Script

---
+++ Aula02/funcoes.js
@@ -0,0 +1,28 @@
+// function MultiplicaNumeros() {
+//     let n1 = 2;
+//     let n2 = 3;
+
+//     console.log(n1 * n2);
+// }
+
+// MultiplicaNumeros(); // Imprime 6
+
+// function MultiplicaComParametros(v1, v2){
+//     console.log(v1 ,"x", v2 ,"=", v1 * v2 )
+// };
+
+// MultiplicaComParametros(10, 20);
+// MultiplicaComParametros(3, 30000);
+// MultiplicaComParametros(6, 60000);
+
+function EquacaoDeBurrinho(x, y, z){
+    let resultado = (x + y) / z;
+    return resultado
+}
+console.log(EquacaoDeBurrinho(2,3,5));
+
+const EquacaoDeTontinhos = (x, y, z, w) => {
+    return resultado = (x + y)*(z + w) / 2;
+}
+
+console.log("O resultado é:",EquacaoDeTontinhos(3, 5, 8, 13));
